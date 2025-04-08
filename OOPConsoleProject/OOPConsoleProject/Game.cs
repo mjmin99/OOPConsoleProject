a5af76674885e8ddcd1b1d469048d99e1158d4b5
mjmin99 <mjmin99@naver.com>
2025-04-08 10:44:50 +0900

    [feat] 기본구조제작
    기본구조를 제작했다.
    게임 클래스와
    상속을 위한 BaseScene 클래스 제작

---
+++ OOPConsoleProject/OOPConsoleProject/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public static class Game
+    {
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curScene;
+
+        private static bool gameOver;
+
+        public static void Run()
+        {
+            while (gameOver == false)
+            { 
+                curScene.Render();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result();
+            }
+        }
+    }
+}
